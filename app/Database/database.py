8b90f48fdd9c1ab88a0345bbc49a57f7485b2abf
josiasdev <francisco.batista67@alu.ufc.br>
2025-02-20 12:33:34 -0300

    Feat: Criação da conexão com o banco de dados.

--- app/Database/database.py
+++ app/Database/database.py
@@ -1,5 +1,5 @@
+from motor.motor_asyncio import AsyncIOMotorClient
 import os
-from pymongo import MongoClient
 from dotenv import load_dotenv
 
 env_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '.env'))
@@ -7,13 +7,12 @@ env_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '.env'))
 load_dotenv(dotenv_path=env_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise ValueError("A variável de ambiente DATABASE_URL não foi definida.")
+DB_NAME = os.getenv("DB_NAME")
 
-client = MongoClient(DATABASE_URL)
+if not DATABASE_URL or not DB_NAME:
+    raise ValueError("Variáveis de ambiente DATABASE_URL ou DB_NAME não configuradas corretamente.")
 
-db_name = os.getenv("DB_NAME", "trabalho_3")  # Ex.: defina DB_NAME no .env ou use "meu_banco" como padrão
-db = client[db_name]
 
-def get_db():
-    return db
+client = AsyncIOMotorClient(DATABASE_URL)
+
+db = client[DB_NAME]
